6ad5d0b8bece2ef9ba386c5e3e7be4f14fcc7072
Vlad Frolov <frolvlad@gmail.com>
2023-04-01 10:32:52 +0200

    feat: added error handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use color_eyre::eyre::WrapErr;
 
 #[derive(Parser)]
 #[clap(author, version, about)]
@@ -10,17 +11,28 @@ enum Command {
     JsonToYaml { filename: std::path::PathBuf },
 }
 
-fn main() {
+fn main() -> color_eyre::eyre::Result<()> {
+    color_eyre::install()?;
     let command = Command::parse();
 
     match command {
         Command::YamlToJson { filename, .. } => {
-            let yaml_value: serde_yaml::Value = serde_yaml::from_reader(std::fs::File::open(&filename).unwrap()).unwrap();
-            println!("{}", serde_json::to_string_pretty(&yaml_value).unwrap());
+            let yaml_value: serde_yaml::Value =
+                serde_yaml::from_reader(std::fs::File::open(&filename).wrap_err_with(|| {
+                    format!("Failed to open {filename:?} file to parse YAML content")
+                })?)
+                .wrap_err_with(|| format!("Failed to parse {filename:?} file as YAML"))?;
+            println!("{}", serde_json::to_string_pretty(&yaml_value)?);
         }
         Command::JsonToYaml { filename } => {
-            let json_value: serde_json::Value = serde_json::from_reader(std::fs::File::open(&filename).unwrap()).unwrap();
-            println!("{}", serde_yaml::to_string(&json_value).unwrap());
+            let json_value: serde_json::Value =
+                serde_json::from_reader(std::fs::File::open(&filename).wrap_err_with(|| {
+                    format!("Failed to open {filename:?} file to parse JSON content")
+                })?)
+                .wrap_err_with(|| format!("Failed to parse {filename:?} file as JSON"))?;
+            println!("{}", serde_yaml::to_string(&json_value)?);
         }
     }
+
+    Ok(())
 }
